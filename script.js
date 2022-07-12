6eddd3cc065566049ba4bccfbfaf0325d788a7f3
nicapos <annikaposadas22@gmail.com>
2022-07-12 23:16:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 
+    function deleteTask(e) {
+        e.parent().remove();
+    }
+
     function addTask() {
         var task = $('#new-task').val();
 
         // Create element for new task
         var elemTask = $('<li></li>').text(task);
-        var editBtn = $('<button></button>').text('Edit');
-        var deleteBtn = $('<button></button>').text('Delete');
+        var editBtn = $('<button></button>');
+        var deleteBtn = $('<button></button>');
+
+        editBtn.text('Edit');
+
+        deleteBtn.text('Delete');
+        deleteBtn.click(() => {deleteTask(deleteBtn)});
 
         elemTask.append(editBtn);
         elemTask.append(deleteBtn);
