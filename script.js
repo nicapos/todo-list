d553f5755061799c3fe1c8e19605d45dc53d6b61
nicapos <annikaposadas22@gmail.com>
2022-07-13 14:21:58 +0800

    Add design

--- script.js
+++ script.js
@@ -4,43 +4,77 @@ $(document).ready(function(){
         e.parent().remove();
     }
 
-    function editTask(e) { // where e = edit button
+    function editTask(e) {  // where e = edit button
         var taskHolder = e.parent().find('span');
         
         let newVal = prompt('Edit task', taskHolder.text());
         taskHolder.text(newVal);
     }
 
-    function addTask() {
-        let task = $('#new-task').val();
+    function addTask(task) {
+        /*
+            TEMPLATE:
+            <li>
+                <button class="far fa-square"><i class="fa fa-check"></i></button>
+                <span>{{ task }}</span>
+                <button class="fa fa-pen"></button>
+                <button class="fa fa-trash"></button>
+            </li>
+        */
 
-        // Create element for new task
         var elemTask = $('<li></li>');
-        var taskHolder = $('<span></span>');
-        var editBtn = $('<button></button>');
-        var deleteBtn = $('<button></button>');
 
-        taskHolder.text(task);
+        // Add checkbox
+        elemTask.append(
+            $('<button></button>')
+            .append(
+                $('<i></i>')
+                .addClass('fa fa-check')
+            )
+            .addClass('far fa-square')
+        );
 
-        editBtn.text('Edit');
-        editBtn.click(() => {editTask(editBtn)});
+        // Add task label
+        elemTask.append(
+            $('<span></span>').text(task)
+        );
 
-        deleteBtn.text('Delete');
-        deleteBtn.click(() => {deleteTask(deleteBtn)});
+        // Add edit button
+        let editBtn = $('<button></button>');
+        elemTask.append(
+            editBtn
+            .addClass('fa fa-pen')
+            .click(() => {editTask(editBtn)})
+        );
 
-        elemTask.append(taskHolder);
-        elemTask.append(editBtn);
-        elemTask.append(deleteBtn);
-
-        $('ul').append(elemTask);
-
-        // Reset field
-        $('#new-task').val('');
+        // Add delete button
+        let deleteBtn = $('<button></button>');
+        elemTask.append(
+            deleteBtn
+            .addClass('fa fa-trash')
+            .click(() => {deleteTask(deleteBtn)})
+        );
+        
+        $('ul').append(elemTask);   // Add all elements to list
+        $('#new-task').val('');     // Reset field
     }
 
     $('#new-task').keypress(function (e) { 
-        if (e.which == 13) // on press Enter
-            addTask();
+        if (e.which == 13) {    // on press Enter
+            let task = $('#new-task').val();
+            addTask(task);
+        }
     });
+
+    /* 
+        From here,
+        setup elements for demo only
+    */
+    addTask('Read a book');
+    addTask('Buy groceries');
+    addTask('Attend meeting at 4pm');
+
+    // Demo checked item for the last item
+    $('li:last-child > button:first-child').addClass('checked');
 
 });
